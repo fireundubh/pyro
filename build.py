bea2db13134402f5cedc3118f2688f8b30cecfbd
fireundubh <fireundubh@gmail.com>
2019-11-14 14:11:05 -0800

    Fixed issue where build.py had to be run from the current working directory Imported specific modules where appropriate Sanitized path attributes

--- build.py
+++ build.py
@@ -1,26 +1,36 @@
-import argparse
-import os
-import shutil
-import subprocess
 import sys
-import zipfile
+from argparse import ArgumentParser, Namespace
 from glob import glob
+from os import makedirs, remove
+from os.path import dirname, exists, isfile, join, normpath, relpath
+from shutil import copy2, rmtree
+from subprocess import call
+from zipfile import ZIP_LZMA, ZipFile
 
 version = '1.3.4'
 
 
 class Application:
-    def __init__(self, args: argparse.Namespace) -> None:
-        self.cwd: str = os.getcwd()
+    def __init__(self, args: Namespace) -> None:
+        self.root_path: str = dirname(__file__)
 
         self.package_name = args.package_name
 
-        self.dist_folder = os.path.normpath(os.path.join(self.cwd, '%s.dist' % self.package_name))
-        self.tools_dir_src = os.path.normpath(os.path.join(os.path.dirname(__file__), 'tools'))
-        self.tools_dir_dst = os.path.normpath(os.path.join(self.dist_folder, 'tools'))
+        self.dist_path = join(self.root_path, '%s.dist' % self.package_name)
+        self.root_tools_path = join(self.root_path, 'tools')
+        self.dist_tools_path = join(self.dist_path, 'tools')
+
+    def __setattr__(self, key, value):
+        # sanitize paths
+        if key.endswith('path'):
+            value = normpath(value)
+            # normpath converts empty paths to os.curdir which we don't want
+            if value == '.':
+                value = ''
+        super(Application, self).__setattr__(key, value)
 
     def _clean_dist_folder(self) -> None:
-        if not os.path.exists(self.dist_folder):
+        if not exists(self.dist_path):
             return
 
         files_to_keep = (
@@ -34,34 +44,34 @@ class Application:
             'etree.pyd'
         )
 
-        files = [f for f in glob(os.path.join(self.dist_folder, '**\*'), recursive=True)
-                 if os.path.isfile(f) and not f.endswith(files_to_keep)]
+        files = [f for f in glob(join(self.dist_path, '**\*'), recursive=True)
+                 if isfile(f) and not f.endswith(files_to_keep)]
 
         for f in files:
-            os.remove(f)
+            remove(f)
             print('Deleted: %s' % f)
 
-        site_dir = os.path.join(self.dist_folder, 'site')
-        if os.path.exists(site_dir):
-            shutil.rmtree(site_dir, ignore_errors=True)
+        site_dir = join(self.dist_path, 'site')
+        if exists(site_dir):
+            rmtree(site_dir, ignore_errors=True)
 
     def _build_zip_archive(self) -> str:
         zip_file: str = '%s_v%s.zip' % (self.package_name, version.replace('.', '-'))
-        zip_path: str = os.path.join(self.cwd, 'bin', zip_file)
-        os.makedirs(os.path.dirname(zip_path), exist_ok=True)
+        zip_path: str = join(self.root_path, 'bin', zip_file)
+        makedirs(dirname(zip_path), exist_ok=True)
 
-        files = [f for f in glob(os.path.join(self.dist_folder, '**\*'), recursive=True) if os.path.isfile(f)]
+        files = [f for f in glob(join(self.dist_path, '**\*'), recursive=True) if isfile(f)]
 
-        with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_LZMA) as z:
+        with ZipFile(zip_path, 'w', compression=ZIP_LZMA) as z:
             for f in files:
-                z.write(f, os.path.join(self.package_name, os.path.relpath(f, self.dist_folder)), compress_type=zipfile.ZIP_LZMA)
+                z.write(f, join(self.package_name, relpath(f, self.dist_path)), compress_type=ZIP_LZMA)
                 print('Added file to archive: %s' % f)
 
         return zip_path
 
     def run(self) -> int:
-        if os.path.exists(self.dist_folder):
-            shutil.rmtree(self.dist_folder, ignore_errors=True)
+        if exists(self.dist_path):
+            rmtree(self.dist_path, ignore_errors=True)
 
         # noinspection PyListCreation
         args: list = [
@@ -80,18 +90,17 @@ class Application:
             '--file-reference-choice=runtime'
         ]
 
-        retcode: int = subprocess.call(args)
+        retcode: int = call(args)
         if retcode != 0:
             return retcode
 
         print('Cleaning up dist folder...')
         self._clean_dist_folder()
 
-        os.makedirs(self.tools_dir_dst, exist_ok=True)
+        print('Copying tools...')
+        makedirs(self.dist_tools_path, exist_ok=True)
         for f in ('bsarch.exe', 'bsarch.license.txt'):
-            shutil.copy2(os.path.join(self.tools_dir_src, f), os.path.join(self.tools_dir_dst, f))
-
-        # shutil.copy2(os.path.join(self.tools_dir_src, 'test.bat'), os.path.join(self.dist_folder, 'test.bat'))
+            copy2(join(self.root_tools_path, f), join(self.dist_tools_path, f))
 
         print('Building archive...')
         zip_created: str = self._build_zip_archive()
@@ -101,6 +110,6 @@ class Application:
 
 
 if __name__ == '__main__':
-    _parser = argparse.ArgumentParser()
+    _parser = ArgumentParser()
     _parser.add_argument('-p', '--package-name', action='store', type=str, default='pyro')
     Application(_parser.parse_args()).run()
