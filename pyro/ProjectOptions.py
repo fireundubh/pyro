9ac4e5cf99767eacfeb4093676ab755e2e9342f0
fireundubh <fireundubh@gmail.com>
2019-11-13 23:25:11 -0800

    Added path validation to user-entered path arguments Moved default flags paths to Project class Moved default registry paths to Project class Moved default search path for BSArch to Project class Simplified game-specific flags and registry path arguments

--- pyro/ProjectOptions.py
+++ pyro/ProjectOptions.py
@@ -9,48 +9,47 @@ class ProjectOptions:
     args: Namespace = field(repr=False, default_factory=Namespace)
 
     # required arguments
+    game_type: str = field(init=False, default_factory=lambda: '')
     input_path: str = field(init=False, default_factory=lambda: '')
 
     # optional arguments
-    game_type: str = field(init=False, default_factory=lambda: '')
     disable_anonymizer: bool = field(init=False, default_factory=bool)
     disable_bsarch: bool = field(init=False, default_factory=bool)
     disable_parallel: bool = field(init=False, default_factory=bool)
 
-    # pyro paths
-    temp_path: str = field(init=False, default_factory=lambda: r'..\temp')
-    bsarch_path: str = field(init=False, default_factory=lambda: r'..\tools\bsarch.exe')
-
-    # shared paths
-    game_path: str = field(init=False, default_factory=lambda: '')
+    # compiler arguments
+    compiler_path: str = field(init=False, default_factory=lambda: r'Papyrus Compiler\PapyrusCompiler.exe')
+    flags_path: str = field(init=False, default_factory=lambda: '')
     source_path: str = field(init=False, default_factory=lambda: r'Data\Scripts\Source')
     base_path: str = field(init=False, default_factory=lambda: r'Data\Scripts\Source\Base')
     user_path: str = field(init=False, default_factory=lambda: r'Data\Scripts\Source\User')
 
-    # compiler paths
-    compiler_path: str = field(init=False, default_factory=lambda: r'Papyrus Compiler\PapyrusCompiler.exe')
+    # game arguments
+    game_path: str = field(init=False, default_factory=lambda: '')
+    registry_path: str = field(init=False, default_factory=lambda: '')
 
-    # flags paths
-    fo4_flags_path: str = field(init=False, default_factory=lambda: '')
-    tesv_flags_path: str = field(init=False, default_factory=lambda: '')
-    sse_flags_path: str = field(init=False, default_factory=lambda: '')
+    # tool arguments
+    bsarch_path: str = field(init=False, default_factory=lambda: '')
 
-    # registry paths
-    fo4_registry_path: str = field(init=False, default_factory=lambda: r'SOFTWARE\WOW6432Node\Bethesda Softworks\Fallout4\Installed Path')
-    tesv_registry_path: str = field(init=False, default_factory=lambda: r'SOFTWARE\WOW6432Node\Bethesda Softworks\Skyrim\Installed Path')
-    sse_registry_path: str = field(init=False, default_factory=lambda: r'SOFTWARE\WOW6432Node\Bethesda Softworks\Skyrim Special Edition\Installed Path')
+    # program arguments
+    temp_path: str = field(init=False, default_factory=lambda: r'..\temp')
 
     def __post_init__(self) -> None:
-        self.fo4_flags_path = os.path.join(self.base_path, 'Institute_Papyrus_Flags.flg')
-        self.tesv_flags_path = os.path.join(self.source_path, 'TESV_Papyrus_Flags.flg')
-        self.sse_flags_path = os.path.join(self.base_path, 'TESV_Papyrus_Flags.flg')
-
         for key in self.__dict__:
             if key == 'args':
                 continue
             try:
                 user_value = getattr(self.args, key)
-                if user_value != getattr(self, key):
+                if user_value and user_value != getattr(self, key):
                     setattr(self, key, user_value)
             except AttributeError:
                 continue
+
+    def __setattr__(self, key, value):
+        # sanitize paths
+        if key.endswith('path'):
+            value = os.path.normpath(value)
+            # normpath converts empty paths to os.curdir which we don't want
+            if value == '.':
+                value = ''
+        super(ProjectOptions, self).__setattr__(key, value)
