ec2a041a81b6a38d87bf602de6265fac609bc301
fireundubh <fireundubh@gmail.com>
2019-11-14 01:37:46 -0800

    Fixed build script - Use 'pipenv run nuitka' - Keep only required modules and libraries - Copy bsarch executable and license to pyro.dist\tools folder - Minor fixups

--- build.py
+++ build.py
@@ -6,7 +6,8 @@ import sys
 import zipfile
 from glob import glob
 
-version='1.3.3'
+version = '1.3.4'
+
 
 class Application:
     def __init__(self, args: argparse.Namespace) -> None:
@@ -14,63 +15,60 @@ class Application:
 
         self.package_name = args.package_name
 
-        if os.path.exists(args.python_path):
-            self.python_path = args.python_path
-        else:
-            raise FileNotFoundError(args.python_path + ' does not exist')
+        self.dist_folder = os.path.normpath(os.path.join(self.cwd, '%s.dist' % self.package_name))
+        self.tools_dir_src = os.path.normpath(os.path.join(os.path.dirname(__file__), 'tools'))
+        self.tools_dir_dst = os.path.normpath(os.path.join(self.dist_folder, 'tools'))
 
-    def _clean_dist_folder(self, path: str) -> None:
-        files_to_keep: tuple = (
-            '%s.exe' % self.package_name,
-            'python37.dll',
-            '_asyncio.pyd',
-            '_ctypes.pyd',
-            '_overlapped.pyd',
-            '_socket.pyd',
-            'select.pyd',
-            'unicodedata.pyd',
-            'etree.pyd',
-            '_elementpath.pyd',
-            '_queue.pyd',
-            '_multiprocessing.pyd'
-        )
-
-        if not os.path.exists(path):
+    def _clean_dist_folder(self) -> None:
+        if not os.path.exists(self.dist_folder):
             return
 
-        files: list = [f for f in glob(os.path.join(path, '**\*'), recursive=True)
-                       if os.path.isfile(f) and not f.endswith(files_to_keep)]
+        files_to_keep = (
+            '%s.exe' % self.package_name,
+            'python37.dll',
+            '_multiprocessing.pyd',
+            '_queue.pyd',
+            '_socket.pyd',
+            'select.pyd',
+            '_elementpath.pyd',
+            'etree.pyd'
+        )
+
+        files = [f for f in glob(os.path.join(self.dist_folder, '**\*'), recursive=True)
+                 if os.path.isfile(f) and not f.endswith(files_to_keep)]
 
         for f in files:
             os.remove(f)
             print('Deleted: %s' % f)
 
-    def _build_zip_archive(self, path: str) -> str:
+        site_dir = os.path.join(self.dist_folder, 'site')
+        if os.path.exists(site_dir):
+            shutil.rmtree(site_dir, ignore_errors=True)
+
+    def _build_zip_archive(self) -> str:
         zip_file: str = '%s_v%s.zip' % (self.package_name, version.replace('.', '-'))
         zip_path: str = os.path.join(self.cwd, 'bin', zip_file)
         os.makedirs(os.path.dirname(zip_path), exist_ok=True)
 
-        files: list = [f for f in glob(os.path.join(path, '**\*'), recursive=True) if os.path.isfile(f)]
+        files = [f for f in glob(os.path.join(self.dist_folder, '**\*'), recursive=True) if os.path.isfile(f)]
 
         with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_LZMA) as z:
             for f in files:
-                z.write(f, os.path.join(self.package_name, os.path.relpath(f, path)), compress_type=zipfile.ZIP_LZMA)
+                z.write(f, os.path.join(self.package_name, os.path.relpath(f, self.dist_folder)), compress_type=zipfile.ZIP_LZMA)
                 print('Added file to archive: %s' % f)
 
         return zip_path
 
     def run(self) -> int:
-        package_path: str = os.path.join(self.cwd, self.package_name)
-
-        dist_folder: str = os.path.join(self.cwd, '%s.dist' % self.package_name)
-        if os.path.exists(dist_folder):
-            shutil.rmtree(dist_folder, ignore_errors=True)
+        if os.path.exists(self.dist_folder):
+            shutil.rmtree(self.dist_folder, ignore_errors=True)
 
         # noinspection PyListCreation
         args: list = [
-            self.python_path,
-            '-m', 'nuitka',
-            '--standalone','pyro_cli',
+            'pipenv',
+            'run',
+            'nuitka',
+            '--standalone', 'pyro',
             '--include-package=pyro',
             '--experimental=use_pefile',
             '--python-flag=no_site',
@@ -78,23 +76,25 @@ class Application:
             '--python-for-scons=%s' % sys.executable,
             '--assume-yes-for-downloads',
             '--plugin-enable=multiprocessing',
-            '--show-progress'
+            '--show-progress',
+            '--file-reference-choice=runtime'
         ]
-
-#        args.append('--output-dir=%s' % self.cwd)
-#        args.append('%s' % package_path)
 
         retcode: int = subprocess.call(args)
         if retcode != 0:
             return retcode
 
-# It seems like we need just about everything
-#        print('Cleaning up dist folder...')
-#        self._clean_dist_folder(dist_folder)
+        print('Cleaning up dist folder...')
+        self._clean_dist_folder()
+
+        os.makedirs(self.tools_dir_dst, exist_ok=True)
+        for f in ('bsarch.exe', 'bsarch.license.txt'):
+            shutil.copy2(os.path.join(self.tools_dir_src, f), os.path.join(self.tools_dir_dst, f))
+
+        # shutil.copy2(os.path.join(self.tools_dir_src, 'test.bat'), os.path.join(self.dist_folder, 'test.bat'))
 
         print('Building archive...')
-        shutil.copyfile(os.path.join('pyro_cli','pyro.ini'),os.path.join(dist_folder,'pyro.ini'))
-        zip_created: str = self._build_zip_archive(dist_folder)
+        zip_created: str = self._build_zip_archive()
         print('Wrote archive: %s' % zip_created)
 
         return 0
@@ -102,6 +102,5 @@ class Application:
 
 if __name__ == '__main__':
     _parser = argparse.ArgumentParser()
-    _parser.add_argument('-n', '--python-path', action='store', type=str, default=sys.executable)
-    _parser.add_argument('-p', '--package-name', action='store', type=str, default='pyro_cli')
+    _parser.add_argument('-p', '--package-name', action='store', type=str, default='pyro')
     Application(_parser.parse_args()).run()
