aa2d224e0cf0bb7e54cc0cfe720244c623bd7f10
fireundubh <fireundubh@gmail.com>
2019-11-22 02:05:49 -0800

    Allowed xmlns attribute to be optional

--- pyro/ElementHelper.py
+++ pyro/ElementHelper.py
@@ -7,12 +7,14 @@ from pyro.Logger import Logger
 
 class ElementHelper(Logger):
     @staticmethod
-    def _get_children(parent_element: etree.ElementBase, tag: str, ns: str = 'PapyrusProject.xsd') -> list:
-        return parent_element.findall('ns:%s' % tag[:-1], {'ns': '%s' % ns})
+    def _get_children(parent_element: etree.ElementBase, tag: str) -> list:
+        namespace = [ns for ns in parent_element.nsmap.values()]
+        return parent_element.findall(tag[:-1]) if not namespace else parent_element.findall('ns:%s' % tag[:-1], {'ns': '%s' % namespace[0]})
 
     @staticmethod
-    def get(parent_element: etree.ElementBase, tag: str, ns: str = 'PapyrusProject.xsd') -> etree.ElementBase:
-        return parent_element.find('ns:%s' % tag, {'ns': '%s' % ns})
+    def get(parent_element: etree.ElementBase, tag: str) -> etree.ElementBase:
+        namespace = [ns for ns in parent_element.nsmap.values()]
+        return parent_element.find(tag) if not namespace else parent_element.find('ns:%s' % tag, {'ns': '%s' % namespace[0]})
 
     @staticmethod
     def get_child_values(parent_element: etree.ElementBase, tag: str) -> list:
