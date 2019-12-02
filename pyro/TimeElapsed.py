70e3a3db7ea448b1ef77fe5b8d95fd2f29adda36
fireundubh <fireundubh@gmail.com>
2019-12-02 07:19:42 -0800

    Fixed issue where TimeElapsed values were not converted from floats to Decimal Added value and average methods Removed print callback method

--- pyro/TimeElapsed.py
+++ pyro/TimeElapsed.py
@@ -1,15 +1,14 @@
 from decimal import Context, Decimal, ROUND_DOWN
-from typing import Callable
 
 
 class TimeElapsed:
     def __init__(self) -> None:
-        context = Context(prec=4, rounding=ROUND_DOWN)
-        self.start_time: Decimal = Decimal(0, context)
-        self.end_time: Decimal = Decimal(0, context)
+        self._context = Context(prec=4, rounding=ROUND_DOWN)
+        self.start_time: float = 0.0
+        self.end_time: float = 0.0
 
-    def print(self, *, callback_func: Callable = None) -> None:
-        if not callback_func:
-            callback_func = print
-        time = '{0:.3f}s'.format(self.end_time - self.start_time)
-        callback_func('Compilation time: %s' % time)
+    def average(self, dividend: int) -> Decimal:
+        return round(Decimal(dividend, self._context) / self.value(), 3)
+
+    def value(self) -> Decimal:
+        return Decimal(self.end_time) - Decimal(self.start_time)
