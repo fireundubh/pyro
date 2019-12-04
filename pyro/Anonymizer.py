b7e14634aa56f9daa80afc2ab75c68c366d5b5b4
fireundubh <fireundubh@gmail.com>
2019-12-04 04:51:09 -0800

    Added anonymization of script paths in headers Added more logging to anonymizer

--- pyro/Anonymizer.py
+++ pyro/Anonymizer.py
@@ -16,19 +16,37 @@ class Anonymizer(Logger):
         try:
             header = PexReader.get_header(path)
         except ValueError:
-            Anonymizer.log.error('Cannot anonymize compiled script due to unknown file magic: "%s"' % path)
+            Anonymizer.log.error('Cannot anonymize script due to unknown file magic: "%s"' % path)
             return
 
         file_path: str = header.script_path.value
         user_name: str = header.user_name.value
         computer_name: str = header.computer_name.value
 
-        if not len(file_path) > 0 and file_path.endswith('.psc') and len(user_name) > 0 and len(computer_name) > 0:
+        if not file_path.casefold().endswith('.psc'):
+            Anonymizer.log.warning('Cannot anonymize script due to invalid file extension: "%s"' % path)
+            return
+
+        if not len(file_path) > 0:
+            Anonymizer.log.warning('Cannot anonymize script due to zero-length file path: "%s"' % path)
+            return
+
+        if not len(user_name) > 0:
+            Anonymizer.log.warning('Cannot anonymize script due to zero-length user name: "%s"' % path)
+            return
+
+        if not len(computer_name) > 0:
+            Anonymizer.log.warning('Cannot anonymize script due to zero-length computer name: "%s"' % path)
             return
 
         with open(path, mode='r+b') as f:
+            f.seek(header.script_path.offset, os.SEEK_SET)
+            f.write(bytes(Anonymizer._randomize_str(header.script_path_size.value), encoding='ascii'))
+
             f.seek(header.user_name.offset, os.SEEK_SET)
             f.write(bytes(Anonymizer._randomize_str(header.user_name_size.value), encoding='ascii'))
 
             f.seek(header.computer_name.offset, os.SEEK_SET)
             f.write(bytes(Anonymizer._randomize_str(header.computer_name_size.value, True), encoding='ascii'))
+
+            Anonymizer.log.info('Anonymized "%s"...' % path)
