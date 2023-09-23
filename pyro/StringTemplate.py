bd7f135f93384e851b4b807a7c4e0c4984ae102e
fireundubh <fireundubh@gmail.com>
2023-09-23 16:33:08 -0700

    Minor enhancement to string template

--- pyro/StringTemplate.py
+++ pyro/StringTemplate.py
@@ -1,5 +1,9 @@
+import re as _re
 from string import Template
 
 
 class StringTemplate(Template):
+    # noinspection PyClassVar
     delimiter: str = '@'
+    idpattern = '([_a-z][_a-z0-9]*)'
+    flags = _re.IGNORECASE | _re.ASCII
