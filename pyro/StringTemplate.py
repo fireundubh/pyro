b10975bb1c2e92713a2acab5bd1f3d8b6479566c
fireundubh <fireundubh@gmail.com>
2025-10-11 20:58:41 -0700

    Fixed issue with StringTemplate reliability

--- pyro/StringTemplate.py
+++ pyro/StringTemplate.py
@@ -4,6 +4,7 @@ from string import Template
 
 class StringTemplate(Template):
     # noinspection PyClassVar
-    delimiter: str = '@'  # type: ignore
-    idpattern = '([_a-z][_a-z0-9]*)'
+    delimiter: str = '@'
+    idpattern = r'([_a-z][_a-z0-9]*)'
     flags = _re.IGNORECASE | _re.ASCII
+    pattern = rf'{_re.escape(delimiter)}(?P<named>{idpattern})'
